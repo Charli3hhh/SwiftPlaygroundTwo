c93fbb5b89910c319684264d7f58bf17e776f7b8
Charles Ombao <charlesb.ombao@gmail.com>
2020-02-04 10:52:31 -1000

    Completed Parts 5 problem set 1-2, continuing problem set 3 for HW

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -12,6 +12,8 @@ else
     print("I need to work harder in this class")
 }
 
+//Part 4: Using Boolean variables in if Statements
+
 var gradeA = true
     
 if gradeA
@@ -21,4 +23,40 @@ if gradeA
 else
 {
     print("I'm not so sure about coding")
+}
+
+//Part 5: On your own
+
+//Problem Set 1:
+
+var Aiea = 4
+var Moanalua = 6
+if Aiea > Moanalua
+{
+    print("true")
+}
+else
+{
+    print("false")
+}
+
+//Problem Set 2:
+
+var DarthVader = "I'm your father, "
+var Luke = "It can't be true "
+if DarthVader == "I'm your father, "
+{
+    let scene = DarthVader + Luke
+    print(scene + "I can't believe it" )
+}
+
+//Problem Set 3:
+var red = true
+if red
+{
+    "red is my favorite color"
+}
+else
+{
+    "red is not my favorite color"
 }
